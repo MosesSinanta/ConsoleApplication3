fb2ce0e45a94252278f3b610e0c4af3b8482e2b3
MosesSinanta <72888489+MosesSinanta@users.noreply.github.com>
2022-07-02 15:04:16 +0700

    Add project files.

---
+++ ConsoleApplication3/Addition.h
@@ -0,0 +1,192 @@
+void display(std::vector<char> vct, int num) {
+	//Declare variables
+	int i;
+
+	//Output
+	for (i = 0; i < vct.size(); i++)
+		std::cout << vct[i];
+	std::cout << std::endl;
+	for (i = 0; i < num - 1; i++)
+		std::cout << " ";
+	std::cout << "^" << std::endl;
+	Sleep(100);
+	//system("CLS");
+}
+
+void q1(std::vector<char> vct, int num);
+void q2(std::vector<char> vct, int num);
+void q3(std::vector<char> vct, int num);
+void q4(std::vector<char> vct, int num);
+void q5(std::vector<char> vct, int num);
+void q6(std::vector<char> vct, int num);
+void q7(std::vector<char> vct, int num);
+void q8(std::vector<char> vct, int num);
+void q9(std::vector<char> vct, int num);
+void q10(std::vector<char> vct, int num);
+void q11(std::vector<char> vct, int num);
+void q12(std::vector<char> vct, int num);
+void fs(std::vector<char> vct, int num) {
+	display(vct, num);
+	std::cout << "We did it!" << std::endl;
+}
+
+//List of states
+void q0(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '1') {
+		num = num + 1;
+		q1(vct, num);
+	}
+}
+
+void q1(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		vct.at(num - 1) = '.';
+		num = num + 1;
+		q2(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		num = num + 1;
+		q8(vct, num);
+	}
+}
+
+void q2(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num + 1;
+		q2(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		num = num + 1;
+		q3(vct, num);
+	}
+}
+
+void q3(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num + 1;
+		q3(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		vct.push_back(' ');
+		num = num + 1;
+		q4(vct, num);
+	}
+}
+
+void q4(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == ' ') {
+		vct.at(num - 1) = '0';
+		num = num - 1;
+		q5(vct, num);
+	}
+	else if (vct[num - 1] == '0') {
+		num = num + 1;
+		q4(vct, num);
+	}
+}
+
+void q5(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num - 1;
+		q5(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		num = num - 1;
+		q6(vct, num);
+	}
+}
+
+void q6(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num - 1;
+		q6(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		num = num - 1;
+		q7(vct, num);
+	}
+}
+
+void q7(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '.') {
+		vct.at(num - 1) = '0';
+		num = num + 1;
+		q1(vct, num);
+	}
+	else if (vct[num - 1] == '0') {
+		num = num - 1;
+		q7(vct, num);
+	}
+}
+
+void q8(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		vct.at(num - 1) = '.';
+		num = num + 1;
+		q9(vct, num);
+	}
+	else if (vct[num] == '1') {
+		num = num + 1;
+		fs(vct, num);
+	}
+}
+
+void q9(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num + 1;
+		q9(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		vct.push_back(' ');
+		num = num + 1;
+		q10(vct, num);
+	}
+}
+
+void q10(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == ' ') {
+		vct.at(num - 1) = '0';
+		num = num - 1;
+		q11(vct, num);
+	}
+	else if (vct[num - 1] == '0') {
+		num = num + 1;
+		q10(vct, num);
+	}
+}
+
+void q11(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '0') {
+		num = num - 1;
+		q11(vct, num);
+	}
+	else if (vct[num - 1] == '1') {
+		num = num - 1;
+		q12(vct, num);
+	}
+}
+
+void q12(std::vector<char> vct, int num) {
+	display(vct, num);
+	if (vct[num - 1] == '.') {
+		vct.at(num - 1) = '0';
+		num = num + 1;
+		q8(vct, num);
+	}
+	else if (vct[num - 1] == '0') {
+		num = num - 1;
+		q12(vct, num);
+	}
+}
---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#ifdef _WIN32
+	#include <Windows.h>
+#else
+	#include <unistd.h>
+#endif
+#include "Addition.h"
+
+int main() {
+	//Declare variables
+	int i;
+	int num{ 1 };
+	std::string str{};
+
+	//Input
+	std::cout << "Enter input:" << std::endl;
+	std::cin >> str;
+	system("CLS");
+
+	//Declare variables
+	std::vector<char> vct;
+	for (i = 0; i < str.length(); i++)
+		vct.push_back(str[i]);
+
+	//Output
+	q0(vct, num);
+	std::cin >> str;
+
+	//Return values
+	return 0;
+}
